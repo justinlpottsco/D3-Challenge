e3b93b16632812890a4668b8e4c127fee5263ec8
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2021-01-13 20:21:14 -0700

    data

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,14 @@
 // YOUR CODE HERE//
+var svgWidth = 1000;
+var svgHeight = 500;
+
+// create an SVG element
+var svg = d3.select("#svg-area")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Load csv data
+d3.csv("data.csv").then(function(healthriskData) {
+
+  console.log(healthriskData);
