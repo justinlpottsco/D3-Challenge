76a90ebdd0246c5b4ff0b4f4ff3eda530f828aeb
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2021-01-13 20:35:11 -0700

    svg

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,14 +1,30 @@
-// YOUR CODE HERE//
+// Setup chart
 var svgWidth = 1000;
 var svgHeight = 500;
 
-// create an SVG element
-var svg = d3.select("#svg-area")
+var margin = {
+    top: 20,
+    right: 40,
+    bottom: 60,
+    left: 50
+};
+// Define dimensions of chart area
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper,
+// append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+
+var svg = d3.select("#circles")
   .append("svg")
   .attr("width", svgWidth)
   .attr("height", svgHeight);
 
-// Load csv data
-d3.csv("data.csv").then(function(healthriskData) {
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  console.log(healthriskData);
+// Import data from csv data file
+d3.csv("data.csv").then(function(healthriskData) {
+    console.log(healthriskData)
+});
