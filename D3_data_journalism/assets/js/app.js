3d5236d480a44bb60685000c90b8fb21ae5c8ff1
justinlpottsco <69825251+justinlpottsco@users.noreply.github.com>
2021-01-21 22:07:10 -0700

    finalnada

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -26,7 +26,7 @@ var chartGroup = svg.append("g")
 // Import data from csv data file
 d3.csv("data.csv").then(function(healthriskData) {
     console.log(healthriskData);
-
+    visualize(healthriskData);
 
   // Format the data
   // healthriskData.forEach(function(data) {
@@ -34,75 +34,75 @@ d3.csv("data.csv").then(function(healthriskData) {
   //     data.poverty = +data.poverty;
 });
 
-// Create Scales
-var xLinearScale = d3.scaleLinear()
-.domain(d3.extent(healthriskData, d => d.poverty))
-.range([0, width]);
+  // Create Scales
+  var xLinearScale = d3.scaleLinear()
+  .domain(d3.extent(healthriskData, d => d.poverty))
+  .range([0, width]);
 
-var yLinearScale = d3.scaleLinear()
-.domain([0, d3.max(healthriskData, d => d.healthcare)])
-.range([height, 0]);
+  var yLinearScale = d3.scaleLinear()
+  .domain([0, d3.max(healthriskData, d => d.healthcare)])
+  .range([height, 0]);
 
 
-// Create axis functions
-var bottomAxis = d3.axisBottom(xLinearScale);
-var leftAxis = d3.axisLeft(yLinearScale);
+  // Create axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
 
-// Append Axis to the chart
-chartGroup.append("g")
-  .attr("transform", `translate(0, ${height})`)
-  .call(bottomAxis);
+  // Append Axis to the chart
+  chartGroup.append("g")
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
 
-chartGroup.append("g")  
-  .call(leftAxis);
+  chartGroup.append("g")  
+    .call(leftAxis);
 
-// Create Circles
-var circlesGroup = chartGroup.selectAll("circle")
-  .data(healthriskData)
-  .enter()
-  .append("circle")
-  .attr("cx", d => xLinearScale(d.poverty))
-  .attr("cy", d => yLinearScale(d.healthcare))
-  .attr("r", "15")
-  .attr("fill", "blue")
-  .attr("opacity", ".5");
+  // Create Circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(healthriskData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", "15")
+    .attr("fill", "blue")
+    .attr("opacity", ".5");
 
-// Initialize tool tip
-var toolTip = d3.tip()
-  .attr("class", "tooltip")
-  .offset([80, -60])
-  .html(function(d) {
-    return (`${d.id}<br>poverty: ${d.poverty}<br>healthcare: ${d.healthcare}`);
+  // Initialize tool tip
+  var toolTip = d3.tip()
+    .attr("class", "tooltip")
+    .offset([80, -60])
+    .html(function(d) {
+      return (`${d.id}<br>poverty: ${d.poverty}<br>healthcare: ${d.healthcare}`);
+    });
+
+  // Create tooltip in the chart
+  chartGroup.call(toolTip);
+
+  // Create event listeners to display and hide the tooltip
+  circlesGroup.on("click", function(data) {
+    toolTip.show(data, this);
+  })
+  // onmouseout event
+    .on("mouseout", function(data) {
+    toolTip.hide(data);
   });
 
-// Create tooltip in the chart
-chartGroup.call(toolTip);
+  // Create axes labels
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left + 40)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("class", "axisText")
+    .text("In Poverty (%)");
 
-// Create event listeners to display and hide the tooltip
-circlesGroup.on("click", function(data) {
-  toolTip.show(data, this);
-})
-// onmouseout event
-  .on("mouseout", function(data) {
-  toolTip.hide(data);
-});
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("class", "axisText")
+    .text("Lacks Healthcare (%)");
+    //.catch(function(error) {
 
-// Create axes labels
-chartGroup.append("text")
-  .attr("transform", "rotate(-90)")
-  .attr("y", 0 - margin.left + 40)
-  .attr("x", 0 - (height / 2))
-  .attr("dy", "1em")
-  .attr("class", "axisText")
-  .text("In Poverty (%)");
-
-chartGroup.append("text")
-  .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
-  .attr("class", "axisText")
-  .text("Lacks Healthcare (%)");
-  //.catch(function(error) {
-
-//console.log(error);
-//});
+  //console.log(error);
+  //});
 
   
